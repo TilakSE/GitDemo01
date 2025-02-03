afedae77c570deeaaea5ed415846c9c5e1a794cb
TilakSE <tilakelango29@gmail.com>
2025-02-03 10:12:01 +0530

    Added a method

--- src/main/java/com/mphasis/GitDemo2/App.java
+++ src/main/java/com/mphasis/GitDemo2/App.java
@@ -6,6 +6,9 @@ package com.mphasis.GitDemo2;
  */
 public class App 
 {
+	public void method1() {
+		System.out.println("This method was added by master");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
